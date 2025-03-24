347ce037460a807295debb232b6913cd5270aac7
Jorge-coder02 <jorgitenis@hotmail.es>
2025-03-24 11:11:38 +0100

    Cambio visual en login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,6 +23,7 @@ function Login({}) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+  const [isPassShown, setIsPassShown] = useState(false);
 
   const handleChange = (e) => {
     dispatch({
@@ -92,32 +93,89 @@ function Login({}) {
       <NavBarLinks ruta_actual="/"></NavBarLinks>
 
       {/* Formulario */}
-      <div className="mt-16 w-full md:w-2/4 pt-12 pb-20 gap-y-6 rounded-lg shadow-lg bg-base-200   ">
+      <div className="mt-16 w-full md:w-2/4 pt-12 pb-20 gap-y-6 rounded-lg shadow-lg bg-base-200">
         <form
-          className="flex flex-col items-center justify-center gap-y-4 [&>div>input]:mb-2 [&>div>input]:rounded-md 
-          [&>div>input]:w-60 [&>div>input]:p-1.5 [&>div>label]:text-lg min-w-full [&>div]:flex 
-          [&>div]:flex-col [&>div]:gap-y-1 md:py-16"
+          className="flex flex-col items-center justify-center gap-y-4 [&_input]:mb-2 [&_input]:rounded-md [&_input]:w-60
+          [&_input]:p-1.5 [&_label]:text-lg min-w-full [&_div]:flex [&>div]:flex-col [&_div]:gap-y-1 [&>div>div]:gap-x-2 md:py-16"
         >
-          <h1 className="text-2xl">Login</h1>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              onChange={handleChange}
-              type="email"
-              id="email"
-              autoComplete="email"
-              value={state.email}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={state.password}
-            />
+          <h1 className="text-3xl">Login</h1>
+          <div className="mt-2 ml-6 flex flex-col [&>div]:flex-col  ">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                onChange={handleChange}
+                type="email"
+                id="email"
+                autoComplete="email"
+                value={state.email}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <div className=" gap-x-2 [&>button]:w-8 [&>button]:p-1">
+                <input
+                  onChange={handleChange}
+                  type={isPassShown ? "text" : "password"}
+                  id="password"
+                  autoComplete="current-password"
+                  value={state.password}
+                />
+                <button
+                  type="button"
+                  onClick={() => setIsPassShown((prev) => !prev)}
+                  className="btn btn-sm btn-primary"
+                >
+                  {!isPassShown ? (
+                    <svg
+                      className="w-4"
+                      fill="none"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M7 6.36185C8.36209 5.55936 10.0212 5 12 5C18.3074 5 21.3671 10.6832 21.9109 11.808C21.9705 11.9311 21.9702 12.0694 21.9107 12.1926C21.5585 12.9208 20.1542 15.5545 17.5 17.3244M14 18.8001C13.3735 18.9286 12.7071 19 12 19C5.69265 19 2.63286 13.3168 2.08909 12.192C2.02953 12.0689 2.03049 11.9291 2.09008 11.8059C2.30875 11.3539 2.9298 10.1741 4 8.92114"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                      />
+                      <path
+                        d="M10 9.76389C10.5308 9.28885 11.2316 9 12 9C13.6569 9 15 10.3431 15 12C15 12.7684 14.7111 13.4692 14.2361 14"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                      />
+                      <path
+                        d="M3 3L21 21"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="feather feather-eye w-4"
+                      fill="none"
+                      height="24"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                      <circle cx="12" cy="12" r="3" />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
           </div>
 
           {/* Create account message */}
